370a4da9f41d4368b2968038530e7044ce2eb39c
sangyi92 <sangyi92@snu.ac.kr>
2016-05-03 00:09:42 +0900

    Update composer.py

--- composer.py
+++ composer.py
@@ -17,19 +17,6 @@ jongTbl = [ 0,      0x3131, 0x3132, 0x3133, 0x3134, 0x3135, 0x3136, 0x3137,
 UniCodeHangulBase = 0xAC00
 UniCodeHangulLast = 0xD79F
 
-choSet = [ 0x3131, 0x3132, 0x3134, 0x3137, 0x3138, 0x3139, 0x3141, 0x3142,
-        0x3143, 0x3145, 0x3146, 0x3147, 0x3148, 0x3149, 0x314a, 0x314b, 0x314c,
-        0x314d, 0x314e ]
-#              ㅏ      ㅐ      ㅑ      ㅒ      ㅓ      ㅔ      ㅕ      ㅖ      ㅗ      ㅘ      ㅙ      ㅚ      ㅛ      ㅜ      ㅝ      ㅞ      ㅟ      ㅠ      ㅡ      ㅢ      ㅣ
-jungSet = [ 0x314f, 0x3150, 0x3151, 0x3152, 0x3153, 0x3154, 0x3155, 0x3156,
-        0x3157, 0x3158, 0x3159, 0x315a, 0x315b, 0x315c, 0x315d, 0x315e, 0x315f,
-        0x3160, 0x3161, 0x3162, 0x3163 ]
-#                      ㄱ      ㄲ      ㄳ      ㄴ      ㄵ      ㄶ      ㄷ      ㄹ      ㄺ      ㄻ      ㄼ      ㄽ      ㄾ      ㄿ      ㅀ      ㅁ      ㅂ      ㅄ      ㅅ      ㅆ      ㅇ      ㅈ      ㅊ      ㅋ      ㅌ      ㅍ      ㅎ
-jongSet = [ 0,      0x3131, 0x3132, 0x3133, 0x3134, 0x3135, 0x3136, 0x3137,
-        0x3139, 0x313a, 0x313b, 0x313c, 0x313d, 0x313e, 0x313f, 0x3140, 0x3141,
-        0x3142, 0x3144, 0x3145, 0x3146, 0x3147, 0x3148, 0x314a, 0x314b, 0x314c,
-        0x314d, 0x314e ]
-
 #                ㄸ        ㅃ      ㅉ  
 ChoOnly = [ 0x3138,  0x3143, 0x3149 ]
 #                   ㄳ      ㄵ      ㄶ       ㄺ      ㄻ      ㄼ      ㄽ      ㄾ      ㄿ      ㅀ        ㅄ    
@@ -52,8 +39,8 @@ def ComposeChar(info):
         info.cho = 0
         return c
     else:
-        c = str(unichr(UniCodeHangulBase + ((choSet.index(info.cho))*21*28) +
-                    (jungSet.index(info.jung))*28 + jongSet.index(info.jong)).encode("utf-8"))
+        c = str(unichr(UniCodeHangulBase + ((choTbl.index(info.cho))*21*28) +
+                    (jungTbl.index(info.jung))*28 + jongTbl.index(info.jong)).encode("utf-8"))
         info.cho = 0
         info.jung = 0
         info.jong = 0
